d3066bf0c612d2dc77bca452b4839a1bd72d1228
Bernard Matanga <bernardmatanga@yahoo.com>
2024-01-31 09:36:44 +0200

    Update SpeakGoodBye.js

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -3,10 +3,8 @@
 (function(window) {
   var byeSpeaker = {};
 
-  byeSpeaker.speakWord = "Good Bye";
-
   byeSpeaker.speak = function(name) {
-    console.log(this.speakWord + " " + name);
+    console.log("Good Bye " + name);
   };
 
   window.byeSpeaker = byeSpeaker;
